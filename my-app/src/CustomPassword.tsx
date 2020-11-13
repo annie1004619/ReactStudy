7410bf47ec4975ea1ae45c7dfbce3e6eaee879f7
annie1004619 <annie1004619@naver.com>
2020-11-13 15:22:10 +0900

    [ADD] 안 올라간 파일 올리기

---
+++ my-app/src/CustomPassword.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import useInput from "./hooks/useInput";
+
+const CustomPassword = () => {
+  // const [password, setPassword] = useState('');
+  //const [confirmPassword, setConfirmPassword] = useState('');
+  const [pwd, setPwd] = useInput({
+    password: "",
+    confirmPassword: "",
+  });
+  const { password, confirmPassword } = pwd;
+  const [passwordOk, setPasswordOk] = useState(false);
+
+  useEffect(() => {
+    if (password !== confirmPassword) {
+      console.log("패스워드가 다릅니다");
+      setPasswordOk(true);
+    } else {
+      setPasswordOk(false);
+    }
+  }, [confirmPassword]);
+
+  /*const passwordChange =(e:any)=>{
+        setPassword(e.target.value);
+    };
+
+    const confirmPasswordChange = (e:any) =>{
+        setConfirmPassword(e.target.value);
+    };*/
+
+  return (
+    <div>
+      <input
+        type="password"
+        value={password}
+        placeholder="비밀번호"
+        onChange={setPwd}
+      />
+      <input
+        type="password"
+        value={confirmPassword}
+        placeholder="비밀번호 확인"
+        onChange={setPwd}
+      />
+      {passwordOk && <p style={{ color: "red" }}>비밀번호가 다릅니다.</p>}
+    </div>
+  );
+};
+
+export default CustomPassword;
